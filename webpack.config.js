e8ce30be8460154f542aa8d83daa014cfc089d77
Don Hamilton III <donhamiltoniii@gmail.com>
2020-01-06 13:20:31 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    modules: [path.resolve('./lib'), path.resolve('./node_modules')]
+  },
+  entry: ['@babel/polyfill', './lib/renderers/dom.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' }
+    ]
+  }
+};
